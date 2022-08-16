dd8cd4fd75ca40379427d4aefa3f5761e1d48371
ehdgusdl9177 <k23129177@gmail.com>
2022-08-16 17:27:29 +0900

    자료형(data type) - 정수의 표현

--- 코딩/Character1/src/ch05/VariableTest.java
+++ 코딩/Character1/src/ch05/VariableTest.java
@@ -7,8 +7,15 @@ public class VariableTest {
 		age = 10;
 		
 		int level = 10000;
+		
+		byte bnum = -128;
+		// int num = 12345678900;
+		long lnum = 12345678900L; 
+		
 		System.out.println(age);
 		System.out.println(level);
+		System.out.println(bnum);
+		System.out.println(lnum);
 	}
 
 }
