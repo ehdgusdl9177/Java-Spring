22ccfc6fc3991306350be42cb107c2a57c9fcdde
ehdgusdl9177 <k23129177@gmail.com>
2022-10-20 09:22:29 +0900

    DELETE API

---
+++ 코딩/delete/src/main/java/com/example/delete/controller/DeleteController.java
@@ -0,0 +1,14 @@
+package com.example.delete.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteController {
+
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account) {
+        System.out.println(userId);
+        System.out.println(account);
+    }
+}
